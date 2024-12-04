70e9489439445b499cef2fa5bce0eafb4e3bde46
privtiburoncin <davidddgv14@gmail.com>
2024-12-04 18:37:14 -0500

    porfa que sea la ultima

--- html/2.conexion.php
+++ html/2.conexion.php
@@ -1,9 +1,10 @@
 <?php
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=nbasegura;charset=utf8mb4', 'root', '');
+    $conn = new PDO('mysql:host=localhost;dbname=nbasegura;charset=utf8mb4', 'root', 'holacomovas');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
+
