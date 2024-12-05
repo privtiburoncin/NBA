3fe7ed5973ee19f2d5e1dc550b252f5eb5df0db1
privtiburoncin <davidddgv14@gmail.com>
2024-12-04 19:01:37 -0500

    ahora si, hora de la verdad

--- html/2.conexion.php
+++ html/2.conexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=nbasegura;charset=utf8mb4', 'usuario', 'tu_contraseña');
+    $conn = new PDO('mysql:host=localhost;dbname=nbasegura;charset=utf8mb4', 'usuario', 'Hola#comovas.12');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
