90e552442ca926f1c2f39e8dd7b3701e14fbc882
privtiburoncin <davidddgv14@gmail.com>
2024-12-04 20:05:17 -0500

    y si mejor retiro la carrera?

--- html/2.conexion.php
+++ html/2.conexion.php
@@ -1,10 +1,9 @@
 <?php
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=nbasegura;charset=utf8mb4', 'usuario', 'Hola#comovas.12');
+    $conn = new PDO('mysql:host=localhost;dbname=nbasegura;charset=utf8mb4', 'root', 'tu_nueva_contraseña');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
-
