d3479f9e340dfa23d6bceca195607c773d1ee85e
privtiburoncin <davidddgv14@gmail.com>
2024-12-09 09:12:06 -0500

    ddsd

--- html/2.conexion.php
+++ html/2.conexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO('mysql:host=201.226.200.24;dbname=nbasegura;charset=utf8mb4', 'usuario', 'Hola#comovas.12');
+    $conn = new PDO('mysql:host=172.29.126.159;dbname=nbasegura;charset=utf8mb4', 'usuario', 'Hola#comovas.12');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 } catch (PDOException $e) {
